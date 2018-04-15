bdfd3e6531ff805c454201b37ccd6a9063b374eb
samching <sam.19@dartmouth.edu>
2018-04-14 22:56:54 -0400

    added starterv2

--- starter.py
+++ starter.py
@@ -13,10 +13,10 @@ from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.externals import joblib
 
-root_dir = ""
-
+root_dir = "."
+    
 wordnet_lemmatizer = WordNetLemmatizer()
 
 
-with open(root_dir + 'yelp.json') as data_file:
+with open(root_dir + 'yelp.json') as data_file:    
     yelp = json.load(data_file)
